65c94c25679012a663aab13a8ac60d3878cd9a22
Nina Prus <nina.prus95@gmail.com>
2021-02-06 13:13:13 +0200

    Made changes

--- hw_3.Functions.py
+++ hw_3.Functions.py
@@ -1,8 +1,7 @@
 # 1) Write a function that emulates the game "rock, scissors, paper"
 # At the entrance, your function accepts your version printed from the console, the computer makes a decision randomly.
-
+import random
 def game_rockscissorspaper():
-    import random
     def determine_the_winner(var1, var2):
         if (var1 == 'rock' and var2 == 'scissors') or + \
                 (var1 == 'paper' and var2 == 'rock') or + \
@@ -13,7 +12,7 @@ def game_rockscissorspaper():
         else:
             print('Computer won')
 
-    var_list = ['rock', 'scissors', 'paper']
+    var_list = ('rock', 'scissors', 'paper')
     var1 = input('Input rock, scissors or paper: ')
     var2 = str(random.choice(var_list))
     print('Choice computer: ', var2)
@@ -30,18 +29,19 @@ game_rockscissorspaper()
 # "people" ⁠— Number of people in the household.
 # "tp" ⁠— Number of rolls.
 # Return a statement telling the user if they need to buy more TP!
-def toilet_paper(**dict):
-    SHEETS_PER_ROLL = 500
-    SHEETS_PER_DAY = 57
-    DAYS = 14
 
+SHEETS_PER_ROLL = 500
+SHEETS_PER_DAY = 57
+DAYS = 14
+
+def toilet_paper(**dict):
     people = int(dict["people"])
     tp = int(dict["tp"])
 
     sheets_for_people = SHEETS_PER_DAY * people
     sheets_for_14_days = sheets_for_people * DAYS
 
-    if sheets_for_14_days % SHEETS_PER_ROLL != 0:
+    if sheets_for_14_days % SHEETS_PER_ROLL:
         need_roll = (sheets_for_14_days // SHEETS_PER_ROLL) + 1
     else:
         need_roll = sheets_for_14_days // SHEETS_PER_ROLL
@@ -75,12 +75,10 @@ toilet_paper(**d)
 # encrypt("karaca") ➞ "0c0r0kaca"
 # encrypt("burak") ➞ "k0r3baca"
 # encrypt("alpaca") ➞ "0c0pl0aca"
-def encrypts():
-    # Input: "apple"
-    word = input('Input word: ')
+def encrypt(word):
 
     # Step 1: Reverse the input: "elppa"
-    word1 = ''.join(item[::-1] for item in word.split())
+    word1 = word[::-1]
 
     # Step 2: Replace all vowels using the following chart:
     d = {'a': 0, 'e': 1, 'i': 2, 'o': 2, 'u': 3}
@@ -88,7 +86,9 @@ def encrypts():
 
     return print(word2)
 
-encrypts()
+word = input('Input word: ')
+
+encrypt(word)
 
 # **4)Given a 3x3 matrix of a completed tic-tac-toe game, create a function that returns whether the game is a win
 # for "X", "O", or a "Draw", where "X" and "O" represent themselves on the matrix, and "E" represents an empty spot.
@@ -149,7 +149,7 @@ def game_tic_tac_toe(matrix):
     determine_the_winner(colm1)
     determine_the_winner(colm2)
 
-    if winX!= True and winO != True: return print('Draw')
+    if not winX and not winO : return print('Draw')
 
 a = [["X", "X", "O"],
      ["X", "1", "O"],
