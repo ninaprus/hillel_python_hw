f7d6a8a9bc8bffad8e955073fe9cc6b20d6b870f
Nina Prus <nina.prus95@gmail.com>
2021-02-09 12:53:33 +0200

    First task completed

--- hw_decorator.py
+++ hw_decorator.py
@@ -1,6 +1,26 @@
 # ЗАДАЧА-1
 # Написать свой декоратор который будет проверять остаток от деления числа 100 на результат работы функции ниже.
 # Если остаток от деления = 0, вывести сообщение "We are OK!», иначе «Bad news guys, we got {}» остаток от деления.
+import functools
+
+def divide_by_100(func):
+    @functools.wraps(func)
+    def wrapper_divide_by_100(*args, **kwargs):
+        value = func(*args, **kwargs)
+        remainder = 100 % value
+        if not remainder:
+            print(f'We are OK!')
+        else:
+            print(f'Bad news guys, we got {remainder}')
+        return value
+    return wrapper_divide_by_100
+
+@divide_by_100
+def math_func(number):
+    res = sum([i for i in range(number)])
+    return res
+
+print(math_func(5))
 
 # ЗАДАЧА-2
 # Написать декоратор который будет выполнять предпроверку типа аргумента который передается в вашу функцию.
