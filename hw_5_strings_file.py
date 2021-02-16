45947b72f8ea0442d751e64811d1838b104da48e
Nina Prus <nina.prus95@gmail.com>
2021-02-16 16:31:40 +0200

    Second task completed

--- hw_5_strings_file.py
+++ hw_5_strings_file.py
@@ -21,3 +21,14 @@ with open('example.txt') as reader:
             line2 = ' '.join([item for item in line if item not in len_3_5[0:-1]])
 
         print(f'{line2}')
+
+#2)Текстовый файл содержит записи о телефонах и их владельцах.
+# Переписать в другой файл телефоны тех владельцев,
+# фамилии которых начинаются с букв К и С.
+
+with open('phone_num.txt', 'r') as reader_ph, open('phone_num_new.txt', 'w') as writer_ph:
+    for line in reader_ph:
+        for item in line:
+            if item.isalpha():
+                if item.startswith('C') or item.startswith('K'):
+                    writer_ph.write(line)
