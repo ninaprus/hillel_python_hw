9e80435d8a9adfa59896115d14ab298bae0f2215
Nina Prus <nina.prus95@gmail.com>
2021-02-20 17:49:42 +0200

    Third task completed

--- hw_7.class.py
+++ hw_7.class.py
@@ -108,3 +108,47 @@ a.merger('example_json_2.json')
 # Вы должны описать каждый из этих атрибутов в виде гетеров и
 # сеттеров(@property). У вас должна быть возможность
 # получения и назначения этих атрибутов в классе.
+
+class Unit:
+
+    def __init__(self, unit_name, mac_address, ip_address, login, password):
+        self._unit_name = unit_name
+        self._mac_address = mac_address
+        self._ip_address = ip_address
+        self._login = login
+        self._password = password
+
+    @property
+    def unit_name(self):
+        return self._unit_name
+    @property
+    def mac_address(self):
+        return self._mac_address
+    @property
+    def ip_address(self):
+        return self._ip_address
+    @property
+    def login(self):
+        return self._login
+    @property
+    def password(self):
+        return self._password
+
+    @unit_name.setter
+    def unit_name(self, new_unit_name):
+        self._unit_name = new_unit_name
+    @mac_address.setter
+    def mac_address(self, new_mac_address):
+        self._mac_address = new_mac_address
+    @ip_address.setter
+    def ip_address(self, new_ip_address):
+        self._ip_address = new_ip_address
+    @login.setter
+    def login(self, new_login):
+        self._login = new_login
+    @password.setter
+    def password(self, new_password):
+        self._password = new_password
+
+unit = Unit('my_name', '00.1A.3F', '10.11.12.13', 'my_login', 'my_password')
+print(unit.login)
